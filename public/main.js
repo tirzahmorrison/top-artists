9e07de03088e5e4cdbca49d2a334ba9762a04049
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-27 20:54:29 -0400

    save

--- public/main.js
+++ public/main.js
@@ -1,19 +1,19 @@
 const BASE_URL = "https://www.last.fm/api/";
-  angular
-    .module("top-artists", [])
-    .controller("mainController", ["$scope","$http",($scope, $http) => {
+angular
+  .module("top-artists", [])
+  .controller("mainController", ["$scope", "$http", ($scope, $http) => {
 
-    }])
+
 
     $http({
       method: "GET",
-      url: + "show/chart.getTopArtists",
-    }).then(response =>{
+      url: + "show/chart.getTopArtists"
+    }).then(response => {
       console.log(response);
       console.log(response.data);
     })
 
-    
+  }])
 
 
 
